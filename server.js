db3718111431584e5fbe0e150d04048de2c182a9
Urijneser <urijneseret@gmail.com>
2025-07-19 15:21:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.post('/tilda-webhook', (req, res) => {
   const domain = referer ? new URL(referer).hostname : null; // Извлекаем имя домена
 
   // Проверяем, разрешён ли домен
-  const allowedDomains = ['performia-seminar.ru']; // Замените на свои домены
+  const allowedDomains = ['performia-seminar.ru' , 'https://performia-cis.ru']; // Замените на свои домены
   if (!allowedDomains.includes(domain)) {
     console.log('Доступ запрещён для домена:', domain);
     return res.status(403).send('Домен не разрешён');
